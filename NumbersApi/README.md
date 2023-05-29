8ebfb2b8d6120b75c222cc776fce335eca58f67b
Qendresa Shala <80218438+Alesa3@users.noreply.github.com>
2023-05-29 09:55:39 +0200

    Add files via upload

---
+++ NumbersApi/README.md
@@ -0,0 +1,16 @@
+# NumbersApi
+
+## Information
+
+This is my first project coding Typescript together with AJAX. 
+
+The API I have selected is the Numbers API (http://numbersapi.com/#42) which allows the user to send their input on a selected year they would like to read interesting facts about. My goal is to build a functional website that sends a query to an open API and displays the response using fetch and DOM manipulation. 
+
+
+## Technologies
+- Typescript
+- HTML
+- CSS
+
+
+
---
+++ NumbersApi/src/app.ts
@@ -0,0 +1,29 @@
+const submitButton = document.querySelector("button") as HTMLButtonElement;
+const displayFact = document.querySelector(".display-fact") as HTMLParagraphElement;
+const yearInput = document.getElementById('year-input') as HTMLInputElement;
+
+const url = "http://numbersapi.com/";
+
+async function getFacts(year: string) {
+    const response = await fetch(url + year + '/year' + '?json');
+    // console.log(url + year + '/year');
+    const facts = await response.json();
+    return facts;
+
+}
+
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    // console.log("hello");
+
+    if (yearInput.value.length > 0) {
+        getFacts(yearInput.value).then((facts) => {
+            displayFact.innerHTML = facts.text;
+
+            yearInput.value = "";
+        });
+
+    }
+
+
+});
